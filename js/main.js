4efdd6c42f4041ad770f71f1047fab68bdc3830a
Iványi István <i.ivanyi@mito.hu>
2014-02-06 10:37:29 +0100

    date setup #2

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 	var  today = new Date(),
 	tomorrow = Math.round(today.setDate(today.getDate() + 2) / 1000);
 
-	var arrive = Date.parse(2014, 2, 31);
+	var arrive = new Date(2014, 2, 28);
 
 	$('.counter').scojs_countdown({
 		until: arrive
