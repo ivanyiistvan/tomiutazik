792f2a821fd9d9d7770e926b6f62c177cae77ccf
iivanyi <ivanyi.id@gmail.com>
2014-02-16 01:16:50 +0100

    set counter & title

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
-(function() {
+(function(){
 
-	var  today = new Date(),
-	tomorrow = Math.round(today.setDate(today.getDate() + 2) / 1000);
+	var arrive = new Date(2014, 2, 28, 12) / 1000;
 
 	$('.counter').scojs_countdown({
-		until: tomorrow
+		until: arrive
 	});
 
-})();
+}());
